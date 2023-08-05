c16c14c3d8925907ad98888f1bc22701f3595f67
laser04 <laser04@gmail.com>
2023-08-05 14:03:38 +0900

    first commit

---
+++ src/main/java/com/wizian/admission/wizianb/WizianBApplication.java
@@ -0,0 +1,13 @@
+package com.wizian.admission.wizianb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizianBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizianBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wizian/admission/wizianb/WizianBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizian.admission.wizianb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizianBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
