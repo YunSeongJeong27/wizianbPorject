c98d04fe176b4cc6b36af0790704d6edbd78830e
laser04 <laser04@gmail.com>
2023-08-07 09:34:28 +0900

    first commit

--- src/main/java/com/wizian/admission/wizianb/WizianBApplication.java
+++ src/main/java/com/wizian/admission/wizianb/WizianBApplication.java
@@ -1,13 +1,25 @@
 package com.wizian.admission.wizianb;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.wizian.admission.wizianb.mapper"})
 public class WizianBApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WizianBApplication.class, args);
 	}
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
 
+		return sessionFactory.getObject();
+	}
 }
---
+++ src/main/java/com/wizian/admission/wizianb/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.wizian.admission.wizianb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("hello","하이헬로우 안녕");
+        return "/index";
+    }
+}
