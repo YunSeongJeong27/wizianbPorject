72c220d36d0f201e71007b406d16c1606f5ded13
laser04 <laser04@gmail.com>
2023-08-16 18:12:40 +0900

    자바 부분 추가

--- src/main/java/com/wizian/admission/wizianb/controller/RecruitmentController.java
+++ src/main/java/com/wizian/admission/wizianb/controller/RecruitmentController.java
@@ -1,17 +1,22 @@
 package com.wizian.admission.wizianb.controller;
 
 
+import com.wizian.admission.wizianb.service.RecruitmentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/recruitment")
 public class RecruitmentController {
 
+    private final RecruitmentService recruitmentService;
+
     @GetMapping("/view")
     public ResponseEntity<?> view(){
-        return ResponseEntity.ok("view");
+        return ResponseEntity.ok(recruitmentService.findAll());
     }
 }
