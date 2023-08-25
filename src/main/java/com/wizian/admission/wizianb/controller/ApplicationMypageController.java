e5c684bb53a87b591c9b205b5f70b5dc6df8bdbc
MSH <seunghyun21c@naver.com>
2023-08-25 17:02:29 +0900

    마승현

--- src/main/java/com/wizian/admission/wizianb/controller/ApplicationMypageController.java
+++ src/main/java/com/wizian/admission/wizianb/controller/ApplicationMypageController.java
@@ -1,32 +1,60 @@
 package com.wizian.admission.wizianb.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wizian.admission.wizianb.domain.ApplicationInfo;
+import com.wizian.admission.wizianb.domain.ApplicationMypage;
+import com.wizian.admission.wizianb.domain.ApplicationWrite;
 import com.wizian.admission.wizianb.service.ApplicationInfoService;
+import com.wizian.admission.wizianb.service.ApplicationMypageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
 public class ApplicationMypageController {
 
-    private final ApplicationInfoService applicationInfoService;
+    private final ApplicationMypageService applicationMypageService;
 
     @GetMapping("/checked")
     public String check(HttpSession session, Model model){
-        model.addAttribute("title","마이페이지");
-        session.getAttribute("loginMemberAll");
-        return null;
+        HashMap<String, List<ApplicationMypage>> map = new HashMap<>();
+        String memberId = (String)session.getAttribute("memberId");
+        if(memberId != null){
+            model.addAttribute("title","마이페이지");
+
+            List<ApplicationMypage> courseNameList =  applicationMypageService.getCourseNameList(memberId);
+            model.addAttribute("courseNameList",courseNameList);
+
+            List<String> applyNo = applicationMypageService.getApplyNo(memberId);
+            for(int i=0; i<applyNo.toArray().length ; i++){
+                map.put(applyNo.get(i), applicationMypageService.getApplyMkList(applyNo.get(i)));
+            }
+            model.addAttribute("mapList",map);
+            return "/application/applicationChecked";
+        }else{
+            return "/application/applicationLogin";
+        }
     }
+
+    @PostMapping("/checked/info")
+    @ResponseBody
+    public ApplicationMypage checkedInfo(@RequestBody ApplicationMypage applicationMypage){
+        String rcrtNo = applicationMypage.getRcrtNo();
+        String applyNo = applicationMypage.getAplyNo();
+
+        List<ApplicationMypage> masterList =  applicationMypageService.getApplyMasterList(rcrtNo, applyNo);
+        System.out.println("masterList = " + masterList);
+
+        // Ajax 호출 결과를 리턴
+        return applicationMypage;
+    }
+
 }
