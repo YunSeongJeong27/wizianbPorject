8c2846119ce29bb5608c398ab91ea13f0a081fbd
jaeeun25 <ljhsc00@naver.com>
2023-08-25 15:31:45 +0900

    2023.08.25 회원페이지 합격자발표 + applicationinfo 부적격사유추가

---
+++ src/main/java/com/wizian/admission/wizianb/service/ApplicationPassService.java
@@ -0,0 +1,17 @@
+package com.wizian.admission.wizianb.service;
+
+import com.wizian.admission.wizianb.domain.ApplicationInfo;
+import com.wizian.admission.wizianb.domain.NoticeMessage;
+import com.wizian.admission.wizianb.domain.Recruitment;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public interface ApplicationPassService {
+        List<Recruitment> courseList(String email);
+        ApplicationInfo findApplication(String email, String rcrtNo);
+        NoticeMessage findNoticeMessage(String docPass, String fnlPass, String rcrtNo);
+
+}
